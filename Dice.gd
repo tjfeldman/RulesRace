5f7d0090cb8926cde887d23038a6535110c7e744
Tyler Feldman <tyler.feldman148@gmail.com>
2025-07-17 00:00:40 -0400

    Removed unnessecary lines of code

--- Dice.gd
+++ Dice.gd
@@ -6,8 +6,6 @@ extends Sprite2D
 
 signal dice_has_rolled(roll: String);
 
-#enum Values {ONE=1, TWO, THREE, JAIL, ESCAPE};
-#var sides : Array[Values] = [Values.ONE, Values.TWO, Values.TWO, Values.THREE, Values.JAIL, Values.ESCAPE];
 var sides : Array[String] = ["1", "2", "2", "3", "Jail", "Escape"];
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -15,12 +13,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		animationPlayer.play("Roll");
 		timer.start();
 		label.visible = false;
-
-func rollDice():
-	var rolled = sides.pick_random();
-	#animate die rolling and show rolled side
-	#dieOutput.text = "Die Rolled: %s" % Values.find_key(rolled);
-	return rolled;
 
 func _on_timer_timeout() -> void:
 	var rolledValue = sides.pick_random();
